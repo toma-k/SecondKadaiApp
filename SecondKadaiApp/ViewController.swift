1aa81ef9a993ab6022ea79e17a94d646dbbf8c68
toma-k <1462053@g.iuhw.ac.jp>
2020-05-17 09:22:13 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by tomakazuki on 2020/05/17.
+//  Copyright © 2020 tomakazuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
